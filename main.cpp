734a7f3a5e4bed34b28f93e1a5914a5181c6cc8e
Jeffrey Abraham <Jeffreyabraham27@gmail.com>
2022-11-19 20:42:33 -0500

    Project finish

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int i = 0;
+    do{
+        int choice;
+        std::cout << "Please enter " << i << " : ";
+        std::cin >> choice;
+
+        if(choice == i){
+            std::cout << "Hey! You weren't supposed to enter " << i << std::endl;
+            break;
+        }
+
+        if(i == 10){
+            std::cout << "Wow, you're more patient then I am, you win!" << std::endl;
+            break;
+        }
+
+        i++;
+    }while(true);
+
     return 0;
 }
